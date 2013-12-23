a5c609f676589164058c5f7bb0cc900a2536dc37
Roman <roman@romanvillegas.com>
2013-12-23 16:07:21 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# Openpay node.js API library
+
+## Installation
+
+`npm install openpay`
+
+## Documentation
+
+Full API documentation available at http://docs.openpay.mx/.
+
+## Overview
+
+```js
+//class
+var Openpay = require('openpay');
+//instantiation
+var openpay = new Openpay(' your merchant id ', ' your private key ', [ isProduction ]);
+//use the api
+openpay.< resource_name >.< method_name >( ... )
+```
+
+All methods accept an optional callback as last argument. 
+
+The callback function should follow the format: function(error, body, response) {...}.
+* error: null if the response status code is 200, 201, 204
+* body: null if the response status code is different from 200, 201, 204
+* response: contains the full http reponse
+
+## Examples
+
+### Creating a customer
+```js
+var newCustomer = {
+  "name":"John",
+  "email":"johndoe@example.com",
+  "last_name":"Doe",
+  "address":{
+    "city":"Queretaro",
+    "state":"Queretaro",
+    "line1":"Calle Morelos no 10",
+    "line2":"col. san pablo",
+    "postal_code":"76000",
+    "country_code":"MX"
+  },
+  "phone_number":"44209087654"
+};
+
+openpay.customers.create(newCustomer, function(error, body, response) {
+    error;    // null if no error occurred (status code == 200||201||204)
+    body;     // contains the object returned if no error occurred (status code == 200||201||204)
+    response; // the full http response
+});
+```
+
+### Charging
+```js
+var newCharge = {
+  "method": "card",
+  "card": {
+    "card_number": "4111111111111111",
+    "holder_name": "John Doe",
+    "expiration_year": "20",
+    "expiration_month": "12",
+    "cvv2": "110",
+  },
+  "amount" : 200.00,
+  "description" : "Service Charge",
+  "order_id" : "oid-00721"
+};
+openpay.charges.create(testCreateCharge, function (error, body, response){
+  // ...
+});
+```
+
+### Payout
+```js
+var payout = {
+  "method": "bank_account",
+  "bank_account":{
+    "clabe":"012298026516924616",
+    "holder_name": "John Doe"
+  },
+  "amount" : 10.50,
+  "description" : "Monthly payment"
+};
+openpay.payouts.create(payout, function (error, body, response){
+  // ...
+});
+```
+
+## Configuration
+
+ * `openpay.setTimeout(20000); // in ms (default is 90000ms)`
+ * `openpay.setMerchantId(' your merchant id ');`
+ * `openpay.setPrivateKey(' your private key ');`
+ * `openpay.setProductionReady(true);`
+
+## Development
+
+To run the tests you'll need your sandbox credentials: merchant id and private key from your [Dashboard](https://sandbox-dashboard.openpay.mx/)):
+
+```bash
+$ npm install -g mocha
+$ npm test
+```
+
+## Author
+
+Originally by [Roman Villegas](https://github.com/abh) (ask@develooper.com). 
---
+++ openpay.js
@@ -0,0 +1,534 @@
+var request = require('request');
+var _ = require('underscore');
+
+Openpay.BASE_URL = 'https://api.openpay.mx';
+Openpay.API_VERSION = '/v1/';
+Openpay.SANDBOX_URL = 'https://sandbox-api.openpay.mx';
+Openpay.SANDBOX_API_VERSION = '/v1/';
+
+
+function Openpay(merchantId, privateKey, isProductionReady) {
+  this.merchantId = merchantId;
+  this.privateKey = privateKey;
+  this.isSandbox = isProductionReady ? false : true;
+  this.timeout = 90000;
+  this.define({isSandbox: this.isSandbox, privateKey: this.privateKey, merchantId: this.merchantId, timeout: this.timeout});
+
+  this.setMerchantId = function (merchantId){
+    this.merchantId = merchantId;
+    this._reDefine();
+  };
+
+  this.setPrivateKey = function (privateKey){
+    this.privateKey = privateKey;
+    this._reDefine();
+  };
+
+  this.setProductionReady = function (isProductionReady){
+    this.isSandbox = !isProductionReady;
+    this._reDefine();
+  };
+
+  this.setTimeout = function (timeout){
+    this.timeout = timeout;
+    this._reDefine();
+  };
+
+  this._reDefine = function(){
+    this.define({isSandbox: this.isSandbox, privateKey: this.privateKey, merchantId: this.merchantId, timeout: this.timeout});
+  }
+}
+
+Openpay.prototype.define = function(baseData) {
+  this.charges = new Charges(baseData);
+  this.payouts = new Payouts(baseData);
+  this.fees = new Fees(baseData);
+  this.plans = new Plans(baseData);
+  this.cards = new Cards(baseData);
+  this.customers = new Customers(baseData);
+  //this.transfers = new Transfers(baseData);
+  //this.bankaccounts = new BankAccounts(baseData);
+};
+
+function Charges(baseData){
+  var baseUrl = baseData.merchantId + '/charges';
+  
+  this.create = function (data, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl,
+      requestData: {method: 'POST', json: data},
+      callback: callback
+    }));
+  };
+
+  this.list = function (data, callback) {
+    var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+    var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + query,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+
+  this.get = function (transactionId, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + transactionId,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+
+  this.refund = function (transactionId, data, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + transactionId + '/refund', 
+      requestData: {method: 'POST', json: data},
+      callback: callback
+    }));
+  };
+}
+
+
+function Payouts(baseData){
+  var baseUrl = baseData.merchantId + '/payouts';
+
+  this.create = function (data, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl, 
+      requestData: {method: 'POST', json: data},
+      callback: callback
+    }));
+  };
+
+  this.list = function (data, callback) {
+    var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+    var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + query,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+
+  this.get = function (transactionId, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + transactionId,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+}
+
+
+function Fees(baseData){
+  var baseUrl = baseData.merchantId + '/fees';
+
+  this.create = function (data, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl, 
+      requestData: {method: 'POST', json: data},
+      callback: callback
+    }));
+  };
+
+  this.list = function (data, callback) {
+    var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+    var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + query,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+}
+
+
+function Customers(baseData){
+  var baseUrl = baseData.merchantId + '/customers';
+
+  this.create = function (data, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl, 
+      requestData: {method: 'POST', json: data},
+      callback: callback
+    }));
+  };
+
+  this.list = function (data, callback) {
+    var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+    var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + query,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+
+  this.get = function (customerId, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + customerId,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+
+  this.update = function (customerId, data, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + customerId,
+      requestData: {method: 'PUT', json: data},
+      callback: callback
+    }));
+  };
+
+  this.delete = function (customerId, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + customerId,
+      requestData: {method: 'DELETE'},
+      callback: callback
+    }));
+  };
+
+  this.charges = {
+    baseUrl : baseData.merchantId + '/customers/',
+
+    create : function (customerId, data, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/charges',
+        requestData: {method: 'POST', json: data},
+        callback: callback
+      }));
+    },
+
+    list : function (customerId, data, callback) {
+      var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+      var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/charges' + query,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    get : function (customerId, transactionId, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/charges/' + transactionId,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    refund : function (customerId, transactionId, data, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/charges/' + transactionId + '/refund', 
+        requestData: {method: 'POST', json: data},
+        callback: callback
+      }));
+    }
+  };
+
+  this.transfers = {
+    baseUrl : baseData.merchantId + '/customers/',
+
+    create : function (customerId, data, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/transfers', 
+        requestData: {method: 'POST', json: data},
+        callback: callback
+      }));
+    },
+
+    list : function (customerId, data, callback) {
+      var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+      var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/transfers' + query,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    get : function (customerId, transactionId, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/transfers/' + transactionId,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    }
+  };
+
+  this.payouts = {
+    baseUrl : baseData.merchantId + '/customers/',
+    
+    create : function (customerId, data, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/payouts',
+        requestData: {method: 'POST', json: data},
+        callback: callback
+      }));
+    },
+
+    list : function (customerId, data, callback) {
+      var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+      var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/payouts' + query,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    get : function (customerId, transactionId, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/payouts/' + transactionId,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    }
+  };
+
+  this.subscriptions = {
+    baseUrl : baseData.merchantId + '/customers/',
+
+    create : function (customerId, data, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/subscriptions',
+        requestData: {method: 'POST', json: data},
+        callback: callback
+      }));
+    },
+
+    list : function (customerId, data, callback) {
+      var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+      var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/subscriptions' + query,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    get : function (customerId, subscriptionId, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/subscriptions/' + subscriptionId,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    update : function (customerId, subscriptionId, data, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/subscriptions/' + subscriptionId,
+        requestData: {method: 'PUT', json: data},
+        callback: callback
+      }));
+    },
+
+    delete : function (customerId, subscriptionId, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/subscriptions/' + subscriptionId,
+        requestData: {method: 'DELETE'},
+        callback: callback
+      }));
+    }
+  };
+
+  this.cards = {
+    baseUrl : baseData.merchantId + '/customers/',
+
+    create : function (customerId, data, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/cards',
+        requestData: {method: 'POST', json: data},
+        callback: callback
+      }));
+    },
+
+    list : function (customerId, data, callback) {
+      var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+      var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/cards' + query,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    get : function (customerId, cardId, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/cards/' + cardId,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    delete : function (customerId, cardId, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/cards/' + cardId,
+        requestData: {method: 'DELETE'},
+        callback: callback
+      }));
+    }
+  };
+
+  this.bankaccounts = {
+    baseUrl : baseData.merchantId + '/customers/',
+
+    create : function (customerId, data, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/bankaccounts', 
+        requestData: {method: 'POST', json: data},
+        callback: callback
+      }));
+    },
+
+    list : function (customerId, data, callback) {
+      var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+      var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/bankaccounts' + query, 
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    get : function (customerId, bankId, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/bankaccounts/' + bankId,
+        requestData: {method: 'GET'},
+        callback: callback
+      }));
+    },
+
+    delete : function (customerId, bankId, callback) {
+      sendRequest(_.extend(baseData, {
+        apiUrl: this.baseUrl + customerId + '/bankaccounts/' + bankId,
+        requestData: {method: 'DELETE'},
+        callback: callback
+      }));
+    }
+  };
+}
+
+
+function Cards(baseData){
+  var baseUrl = baseData.merchantId + '/cards';
+
+  this.create = function (data, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl, 
+      requestData: {method: 'POST', json: data},
+      callback: callback
+    }));
+  };
+
+  this.list = function (data, callback) {
+    var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+    var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + query,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+
+  this.get = function (cardId, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + cardId,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+
+  this.delete = function (cardId, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + cardId,
+      requestData: {method: 'DELETE'},
+      callback: callback
+    }));
+  };
+}
+
+function Plans(baseData){
+  var baseUrl = baseData.merchantId + '/plans';
+
+  this.create = function (data, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl, 
+      requestData: {method: 'POST', json: data},
+      callback: callback
+    }));
+  };
+
+  this.list = function (data, callback) {
+    var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+    var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + query,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+
+  this.get = function (planId, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + planId,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+
+  this.update = function (planId, data, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + planId,
+      requestData: {method: 'PUT', json: data},
+      callback: callback
+    }));
+  };
+
+  this.delete = function (planId, callback) {
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + planId,
+      requestData: {method: 'DELETE'},
+      callback: callback
+    }));
+  };
+
+  this.listSubscriptions = function (planId, data, callback) {
+    var query = (data && _.isObject(data) && !_.isArray(data) && !_.isFunction(data) && !_.isEmpty(data)) ? stringifyParams(data) : '';
+    var callback = _.isFunction(callback) ? callback : _.isFunction(data) ? data : null;
+    sendRequest(_.extend(baseData, {
+      apiUrl: baseUrl + '/' + planId + '/subscriptions' + query,
+      requestData: {method: 'GET'},
+      callback: callback
+    }));
+  };
+}
+
+
+var stringifyParams = function (params) {
+  return '?' + _.map(_.pairs(params), function(arr){ return arr.join('='); }).join('&');
+}
+
+var sendRequest = function (data) {
+  var baseUrl = data.isSandbox ? Openpay.SANDBOX_URL + Openpay.SANDBOX_API_VERSION : Openpay.BASE_URL + Openpay.API_VERSION;
+  var baseRequestOptions = {
+    url: baseUrl + data.apiUrl,
+    auth: {
+      username: data.privateKey,
+      pass: ''
+    },
+    method: 'GET',
+    json: true,
+    timeout: data.timeout
+  };
+  //console.log(data.apiUrl);  //console.log(_.extend(baseRequestOptions, data.requestData));
+  var data = _.extend({}, data);
+  request(_.extend(baseRequestOptions, data.requestData), function(err, res, body){
+    var resCode = res.statusCode;
+    var error = (resCode!=200 && resCode!=201 && resCode!=204) ? body : null;
+    data.callback(error, error ? null : body, res);
+  });
+}
+//request('http://www.google.com', function (error, response, body) {})
+
+module.exports = Openpay;
